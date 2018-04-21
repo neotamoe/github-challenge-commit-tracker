8149117f295bbfdc62cfeeabfa36ca7d96e53d6b
Neota Moe <neota.moe@gmail.com>
2018-04-20 20:59:09 -0500

    changed format and simplified dates

--- client/src/components/AddChallenge.vue
+++ client/src/components/AddChallenge.vue
@@ -1,53 +1,65 @@
 <template>
     <div class="hero">
         <div class="hero-body">
-            <h1 class="title is-3">Add Challenge</h1>            
-            <div class="field">
-                <label class="label">Enter Challenge Name</label>
-                <div class="control">
-                    <input  v-model="challenge_name" class="input" type="text" placeholder="i.e. post-Psi, April 2018">
+            <h1 class="title is-3">Challenge Information</h1>            
+            <div v-if="!challengeEntered">
+                <div class="field">
+                    <label class="label">Enter Challenge Name</label>
+                    <div class="control">
+                        <input  v-model="challenge_name" class="input" type="text" placeholder="i.e. post-Psi, April 2018">
+                    </div>
                 </div>
-            </div>
-            <div class="field">
-                <label class="label">Enter Start Date</label>
-                <div class="control">
-                    <input class="input" type="date" placeholder="MM/DD/YYYY" v-model="start_date">
+                <div class="field">
+                    <label class="label">Enter Start Date</label>
+                    <div class="control">
+                        <input class="input" type="date" placeholder="MM/DD/YYYY" v-model="start_date">
+                    </div>
                 </div>
-            </div>
-            <div class="field">
-                <button class="button is-primary" @click="addChallenge()">Add Challenge</button>
+                <div class="field">
+                    <label class="label">Enter End Date</label>
+                    <div class="control">
+                        <input class="input" type="date" placeholder="MM/DD/YYYY" v-model="end_date">
+                    </div>
+                </div>
+                <div class="field">
+                    <button class="button is-primary" @click="addChallenge()">OK</button>
+                </div>
             </div>
             <div class="field" v-if="challengeEntered">
                 <p>Challenge Name: {{challenge_name}}</p>
-                <p>Start Date: {{convertStartDate()}}</p>
+                <p>Start Date: {{start_date}}</p>
                 <p>End Date: {{end_date}}</p>
-            </div>
-
-            <h1 class="title is-3">Add Participant</h1>            
-
-            <div class="field">
-                <label class="label">First Name</label>
-                <div class="control">
-                    <input class="input" type="text" placeholder="e.g Neota" v-model="first_name">
+                <div class="field">
+                    <button class="button is-primary" @click="editChallenge()">Edit</button>
                 </div>
             </div>
+            <div v-if="challengeEntered">
+                <h1 class="title is-3">Add Participant</h1>            
 
-            <div class="field">
-                <label class="label">Last Name</label>
-                <div class="control">
-                    <input class="input" type="text" placeholder="e.g Moe" v-model="last_name">
+                <div class="field">
+                    <label class="label">First Name</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="e.g Neota" v-model="first_name">
+                    </div>
                 </div>
-            </div>
 
-            <div class="field">
-                <label class="label">Github UserName</label>
-                <div class="control">
-                    <input class="input" type="email" placeholder="e.g. neotamoe" v-model="github_username">
+                <div class="field">
+                    <label class="label">Last Name</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="e.g Moe" v-model="last_name">
+                    </div>
                 </div>
-            </div>
-            
-            <div class="field">
-                <button class="button is-primary" @click="addParticipant()">Add Name</button>
+
+                <div class="field">
+                    <label class="label">Github UserName</label>
+                    <div class="control">
+                        <input class="input" type="email" placeholder="e.g. neotamoe" v-model="github_username">
+                    </div>
+                </div>
+                
+                <div class="field">
+                    <button class="button is-primary" @click="addParticipant()">Add Name</button>
+                </div>
             </div>
         </div>
     </div>
@@ -75,11 +87,7 @@ export default {
     methods: {
         async addChallenge () {
             console.log('add challenge button clicked: name: '+ this.challenge_name);
-            this.challengeEntered = true;
-            this.$nextTick(function () {
-              console.log('this should update start_date');
-            });
-   
+            this.challengeEntered = true;   
             // await ChallengeService.addChallenge({
             //     name: this.name,
             //     start_date: this.start_date,
@@ -87,9 +95,7 @@ export default {
             //     current: this.current
             // })
         // this.$router.push({ name: 'Home' })
-            this.end_date = this.getEndDate();
-            var start = this.start_date;
-            this.name = '';
+            // this.end_date = this.getEndDate();
         },
         addParticipant() {
             let participant = {
@@ -105,16 +111,8 @@ export default {
             this.last_name = '';
             this.github_username = '';
         },
-        getEndDate() {          
-            var future = new Date(this.start_date);
-            console.log('future before manipulation (should equal start date)' + future);
-            future.setDate(future.getDate() + 30);
-            console.log(future);
-            return future.toLocaleDateString('en-US');
-        },
-        convertStartDate() {
-            var start = new Date(this.start_date);
-            return start.toLocaleDateString('en-US');
+        editChallenge() {
+            this.challengeEntered = false;
         }
     }
 }
