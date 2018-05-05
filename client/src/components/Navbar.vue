9727493621e6c69408699489e89ca31df97a5fe8
Neota Moe <neota.moe@gmail.com>
2018-05-05 12:25:37 -0500

    changed navbar to have burger menu on mobile

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,37 +1,54 @@
 <template>
-<nav class="navbar">
-  <div class="navbar-brand">
-  </div>
-  <div id="navbarExample" data-target="navMenu" class="navbar-menu">
+  <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-        <span class="navbar-item icon is-large"><i class="fab fa-github"></i></span>
+      <span class="navbar-item icon is-large"><i class="fab fa-github"></i></span>
+        <a class="navbar-burger" data-target="navbarMenu" aria-label="menu" aria-expanded="false" @click="toggleBurger()">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </a>
     </div>
-    <div class="navbar-start">
-      <router-link class="navbar-item" to="/">
-        Home
-      </router-link>
-      <router-link class="navbar-item" to="/posts">
-        Posts
-      </router-link>
-      <router-link class="navbar-item" to="/challenges/current">
-        Current Challenge
-      </router-link>
-      <router-link class="navbar-item" to="/challenges/add">
-        Add a Challenge
-      </router-link>
-      <router-link class="navbar-item" to="/participants/add">
-        Add Participants
-      </router-link>
-      <router-link class="navbar-item" to="/challenges/past">
-        Past Challenges
-      </router-link>
+    <div id="navbarMenu" class="navbar-menu">
+      <div class="navbar-start">
+        <router-link class="navbar-item" to="/">
+          Home
+        </router-link>
+        <router-link class="navbar-item" to="/posts">
+          Posts
+        </router-link>
+        <router-link class="navbar-item" to="/challenges/current">
+          Current Challenge
+        </router-link>
+        <router-link class="navbar-item" to="/challenges/add">
+          Add a Challenge
+        </router-link>
+        <router-link class="navbar-item" to="/participants/add">
+          Add Participants
+        </router-link>
+        <router-link class="navbar-item" to="/challenges/past">
+          Past Challenges
+        </router-link>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
 <script>
 export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    methods: {
+      toggleBurger() {
+        document.querySelector('.navbar-menu').classList.toggle('is-active');
+      } 
+    }
 }
 </script>
+
+<style>
+a.navbar-item {
+  text-align: right
+}
+</style>
